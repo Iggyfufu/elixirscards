16474a25f9824f8fc486f4ff780d8e83a8c58e85
Kenneth Zhu <kennethzhu07@gmail.com>
2019-11-26 14:10:09 -0500

    Add docs and pipe function

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,4 +1,11 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
+  @doc """
+    Returns a list of strings for a deck of cards
+  """
   def create_deck do
     values = [
       "Ace",
@@ -23,27 +30,51 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Return a deck that is shuffled
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Checks if card is within the deck
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck. The `hand_size` argument indicates how many cards should be in the hand.
+  """
   def deal(deck, number) do
     Enum.split(deck, number)
   end
 
+  @doc """
+    Saves deck to a file
+  """
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write!(filename, binary)
   end
 
+  @doc """
+    Loads deck from stored deck
+  """
   def load(filename) do
     case File.read(filename) do
       {:ok, binary} -> :erlang.binary_to_term(binary)
       {:error, _reason} -> "That file does not exist"
     end
+  end
+
+  @doc """
+    Creates a deck shuffles and deals
+  """
+  def create_hand(size) do
+    create_deck()
+    |> shuffle()
+    |> deal(size)
   end
 end
